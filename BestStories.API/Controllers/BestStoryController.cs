c7a32db8a9383ea52a7b089d6438b761df04c177
Saurabh Dash <saurabhcet@gmail.com>
2024-02-08 11:06:45 +0530

    refactor

--- BestStories.API/Controllers/BestStoryController.cs
+++ BestStories.API/Controllers/BestStoryController.cs
@@ -1,7 +1,5 @@
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Caching.Memory;
-using System.Text.Json;
+using System.Text;
 
 namespace BestStories.API.Controllers
 {
@@ -24,14 +22,18 @@ namespace BestStories.API.Controllers
         }
 
         [HttpGet("{n}")]
-        public async Task<ActionResult<IEnumerable<Story>>> GetBestStories2(int n)
+        public async Task<ActionResult<IEnumerable<Story>>> GetBestStories(int n)
         {
             try
             {
                 if (n > _maxAllowedStories)
-                {
-                    _logger.LogWarning($"User is trying to abuse the API, header details:{HttpContext.Request.Headers}");
-                    return Problem($"Sorry we couldn't process your response, maximum allowed best stories: {_maxAllowedStories}");
+                {                    
+                    StringBuilder sbHeaders = new StringBuilder();                    
+                    foreach (var header in HttpContext.Request.Headers)                    
+                        sbHeaders.AppendLine($"{header.Key}:{header.Value}");
+                    
+                    _logger.LogError($"User is trying to abuse the API, header details:{sbHeaders.ToString()}");
+                    throw new InvalidOperationException($"Sorry we couldn't process your response, maximum allowed best stories: {_maxAllowedStories}");
                 }
 
                 var stories = await _hackerNewsService.GetBestStories(n);
@@ -42,70 +44,6 @@ namespace BestStories.API.Controllers
             {                
                 return Problem(ex.Message);
             }
-        }
-
-        //[HttpGet("{n}")]
-        //public async Task<ActionResult> GetBestStories(int n)
-        //{
-        //    var bestStories = new List<Story>();
-
-        //    if(n > _maxAllowedStories)
-        //    {
-        //        _logger.LogWarning($"User is trying to abuse the API, header details:{HttpContext.Request.Headers}");
-        //        return NotFound($"Sorry we couldn't process your response, maximum allowed best stories: {_maxAllowedStories}");
-        //    }
-
-        //    //check if the result exists in cache for count n to avoid hitting hacker-news API
-        //    if (_memoryCache.TryGetValue(BEST_STORIES, out List<Story> cachedStories) && cachedStories?.Count > n)
-        //    {
-        //        return Ok(cachedStories?.Take(n));
-        //    }
-
-        //    //get the story Ids from base API
-        //    var storyIdsResponse = await _httpClient.GetAsync(_configuration[HACKERAPI_STORYIDS_CONFIG]);          
-        //    if (!storyIdsResponse.IsSuccessStatusCode)
-        //    {
-        //        _logger.LogError($"\"Couldn't connect to story provider API, \n url{n}");
-        //        return NotFound("Something went wrong and we couldn't connect to our service provider, please try after some time.");
-        //    }                
-        //    var bestStoryIds = await JsonSerializer.DeserializeAsync<List<int>>(await storyIdsResponse.Content.ReadAsStreamAsync()).ConfigureAwait(false);
-        //    if (bestStoryIds != null && bestStoryIds.Any())
-        //    {
-
-        //        //Exclude the subset from the list which are available in cache
-        //        int actualN = n;
-        //        var bestStoryIdsAfterCacheExclusion = bestStoryIds;
-        //        if (cachedStories!= null && cachedStories.Any())
-        //        {
-        //            bestStoryIdsAfterCacheExclusion = bestStoryIds.Where(num => !cachedStories.Select(x => x.Id).Contains(num)).ToList();
-        //            actualN = n - cachedStories.Count;
-        //        }
-
-        //        foreach (var id in bestStoryIdsAfterCacheExclusion.Take(actualN))
-        //        {
-        //            //get story details for every id                    
-        //            var storyResponse = await _httpClient.GetAsync(_configuration[HACKERAPI_STORYDETAIL_CONFIG]?.Replace("*", id.ToString())).ConfigureAwait(false);
-        //            if (!storyResponse.IsSuccessStatusCode)
-        //            {
-        //                _logger.LogError($"Error response code from story provider API for story Id:{id}, status code:{storyResponse.StatusCode}, url:{id}");
-        //                continue;
-        //            }                       
-        //            var story = await JsonSerializer.DeserializeAsync<Story>(await storyResponse.Content.ReadAsStreamAsync()).ConfigureAwait(false);
-        //            if (story != null)                    
-        //                bestStories.Add(story);                                        
-        //        }
-        //    }
-        //    if (cachedStories != null && cachedStories.Any())
-        //    {
-        //         bestStories.AddRange(cachedStories);
-        //    }
-
-        //    var sortedBestStories = bestStories.OrderByDescending(x => x.Score).ToList();            
-            
-        //    //Refresh the cache every 24 hr(adjust the expiration time based on needs)
-        //    _memoryCache.Set(BEST_STORIES, sortedBestStories, TimeSpan.FromDays(1));
-
-        //    return Ok(sortedBestStories);
-        //}
+        }        
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# BestStories.API
+#BestStories.API
+
+# Compilation Steps:
+1. Open the project in Visual Studio
+2. Check your .net Target framework and update as required.
+3. Compile the solution, select profile BestStories.API and run the application
+4. Launch swagger url - http://localhost:5000/swagger/index.html
+
+
+#Implementation & Assumptions
+1. External Uri and details are config driven.
+1. Restrict the API to a Max ceiling to avoid hacker-news abuse.
+2. Maintain a cache and refresh after 24hrs - only hit the story detail API for ids which are unavailable in the cache. 
+
+#Scope of Improvement 
+1. Decoupling of cache implementation from GetBestStories method
+2. Correct Error code and logging as per the exception type
